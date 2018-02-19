1b00902dbb8c00f1491aa0aefa9569bb51323f70
Takeru Sato <type.in.type@gmail.com>
2018-02-19 21:45:07 +0900

    Upgrade to embulk 0.9.4

--- lib/embulk/output/aerospike.rb
+++ lib/embulk/output/aerospike.rb
@@ -1,3 +1,3 @@
 Embulk::JavaPlugin.register_output(
-  "aerospike", "org.embulk.output.aerospike.AerospikeOutputPlugin",
+  "aerospike", "org.embulk.output.aerospike.AerospikeOutputPlugin$",
   File.expand_path('../../../../classpath', __FILE__))
